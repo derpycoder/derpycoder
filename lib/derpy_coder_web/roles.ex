06aa512256182ea2a4fb48548770962970887714
Abhijit Kar <abhijit.kar@derpycoder.com>
2022-07-12 22:03:04 +0530

    Minor Example Code

--- lib/derpy_coder_web/roles.ex
+++ lib/derpy_coder_web/roles.ex
@@ -12,10 +12,15 @@ defmodule DerpyCoderWeb.Roles do
   @spec can?(User.t(), entity(), action()) :: boolean()
   def can?(user, entity, action)
   def can?(%User{role: :admin}, %{}, _), do: true
+
   def can?(%User{}, %Photo{}, :index), do: true
   def can?(%User{}, %Photo{}, :new), do: true
   def can?(%User{}, %Photo{}, :show), do: true
+  # def can?(%User{}, %Photo{}, action) when action in ~w[index new show]a, do: true
+
   def can?(%User{id: id}, %Photo{user_id: id}, :edit), do: true
   def can?(%User{id: id}, %Photo{user_id: id}, :delete), do: true
+  # def can?(%User{id: id}, %Photo{user_id: id}, action) when action in ~w[edit delete]a, do: true
+
   def can?(_, _, _), do: false
 end
