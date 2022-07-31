e0c5d86edd683c210f835a6bb2e34856b8b1bb71
Abhijit Kar <abhijit.kar@derpycoder.com>
2022-08-01 00:34:50 +0530

    Added iex helper

---
+++ .iex.exs
@@ -0,0 +1,14 @@
+alias DerpyCoderWeb.Router.Helpers, as: Routes
+alias DerpyCoder.Accounts
+alias DerpyCoder.Repo
+
+IO.puts("Started IEx")
+
+defmodule Helpers do
+  @moduledoc """
+  Can be used to reduce typing in IEx.
+  """
+  def init do
+    IO.puts("Initializing...")
+  end
+end
